c6bb11ab4b5c6454371970c0c1593b8ca1cf03b6
RafaelDesignBE <thegoldenboxmail@gmail.com>
2019-06-12 18:02:43 +0200

    Update using native YouTube PIP button

--- auto.js
+++ auto.js
@@ -1,10 +1,13 @@
-document.querySelector('.ytp-miniplayer-button').addEventListener('click',function () {
-	if (!document.pictureInPictureElement) {
-		document.querySelector('video').requestPictureInPicture()
-	}
-});
+// document.querySelector('.ytp-miniplayer-button').addEventListener('click',function () {
+// 	if (!document.pictureInPictureElement) {
+// 		document.querySelector('video').requestPictureInPicture()
+// 	}
+// });
 
-document.querySelector('.ytp-miniplayer-scrim').addEventListener('click',function () {
-		// alert('test')
-		document.exitPictureInPicture()
-});
+// document.querySelector('.ytp-miniplayer-scrim').addEventListener('click',function () {
+// 		// alert('test')
+// 		document.exitPictureInPicture()
+// });
+
+document.querySelector('.ytp-miniplayer-button').style.display = "none";
+document.querySelector('.ytp-pip-button').style.display = "inline-block";
